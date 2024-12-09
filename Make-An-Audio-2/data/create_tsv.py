eb4b0e74ef0f18884ec64022cde2302a93682f6b
Bhargav Hadya <bhadya@andrew.cmu.edu>
2024-12-08 20:47:50 -0500

    updated data files

---
+++ Make-An-Audio-2/data/create_tsv.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+
+def create_tsv_train(filename):
+	# Load the data
+	data = pd.read_csv(filename)
+
+	# Columns: Name, Dataset, Caption, Audio Path, Duration, mel_path
+
+	# Create a new dataframe
+	df = pd.DataFrame(columns=['name', 'dataset', 'caption', 'audio_path', 'duration', 'mel_path'])
+
+	# Iterate over the data
+	for i in range(len(data)):
+		# Get the duration
+		duration = 10.0
+		# Get the caption
+		caption = data['Caption'][i].replace('""', '')
+		# Get the name
+		name = data['Audio_wav'][i]
+		audio_path = f'data/audio_wav_train/{name}'
+		name_mel = name.replace('.wav', '_mel')
+		mel_path = f'data/audio_wav_train_mel/{name_mel}.npy'
+		# Get the dataset
+		dataset = "audiocaps"
+		
+		# Append to the dataframe
+		df = df._append({'name': name, 'dataset': dataset, 'caption': caption, 'audio_path': audio_path, 'duration': duration, 'mel_path': mel_path}, ignore_index=True)
+
+	# Save the dataframe
+	df.to_csv('train.tsv', sep='\t', index=False)
+
+	#Add column named struct_cap to the dataframe, fill with data[Structured Caption]
+	df['struct_cap'] = data['Structured_Caption']
+
+	# Save the dataframe
+	df.to_csv('train_struct.tsv', sep='\t', index=False)
+
+
+def create_tsv_test(filename):
+	#columns: name, dataset, ori_cap, audio_path, mel_path, caption
+	# Load the data
+	data = pd.read_csv(filename)
+
+	df = pd.DataFrame(columns=['name', 'dataset', 'ori_cap', 'audio_path', 'mel_path', 'caption'])
+
+	for i in range(len(data)):
+		# Get the caption
+		caption = data['Caption'][i].replace('""', '')
+		# Get the name
+		name = data['Audio_wav'][i]
+		audio_path = f'data/audio_wav_test/{name}'
+		name_mel = name.replace('.wav', '_mel')
+		mel_path = f'data/audio_wav_test_mel/{name_mel}.npy'
+		# Get the dataset
+		dataset = "audiocaps"
+		struct_cap = data['Structured_Caption'][i]
+		
+		# Append to the dataframe
+		df = df._append({'name': name, 'dataset': dataset, 'ori_cap': caption, 'audio_path': audio_path, 'mel_path': mel_path, 'caption': struct_cap}, ignore_index=True)
+
+	# Save the dataframe
+	df.to_csv('test.tsv', sep='\t', index=False)	
+
+
+if __name__ == '__main__':
+	create_tsv_train('../../creating_structured_prompts/train_df.csv')
+	create_tsv_test('../../creating_structured_prompts/test_df.csv')
